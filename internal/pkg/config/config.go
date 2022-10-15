325270dd37bf74d706a3f56218f9ce7754d50563
Thiago Rotondo Sampaio <thiagorot@gmail.com>
2022-10-15 18:07:06 -0300

    fix(config): fix priority and set default values

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -26,13 +27,32 @@ type AppConfig struct {
 }
 
 func GetConfig() AppConfig {
+	viper.SetDefault("server.host", "localhost")
+	viper.SetDefault("server.port", 4000)
+	viper.SetDefault("database.type", "postgres")
+	viper.SetDefault("database.user", "postgres")
+	viper.SetDefault("database.host", "localhost")
+	viper.SetDefault("database.port", 5432)
+
 	viper.AddConfigPath(".")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yml")
-	viper.AutomaticEnv()
+
+	viper.SetEnvPrefix("app")
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+
+	viper.BindEnv("server.host")
+	viper.BindEnv("server.port")
+	viper.BindEnv("database.type")
+	viper.BindEnv("database.user")
+	viper.BindEnv("database.pass")
+	viper.BindEnv("database.host")
+	viper.BindEnv("database.port")
+	viper.BindEnv("database.name")
 
 	if err := viper.ReadInConfig(); err != nil {
-		log.Fatal("Error loading config.yml file", err)
+		log.Println(err)
 	}
 
 	var config AppConfig
